0ab8aebc81173ae61815f9d7e0f820f756d7231e
Vincent Roth <vroth@sqli.com>
2018-10-17 11:39:47 +0200

    Book service

--- src/app/consultation/book-display/book-display.component.ts
+++ src/app/consultation/book-display/book-display.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from 'src/app/shared/book';
+import { BookService } from 'src/app/shared/book.service';
 
 @Component({
   selector: 'app-book-display',
@@ -9,12 +10,9 @@ import { Book } from 'src/app/shared/book';
 export class BookDisplayComponent implements OnInit {
   private book: Book;
 
-  constructor() {}
+  constructor(private bookService: BookService) {}
 
   ngOnInit() {
-    this.book = {
-      title: 'Title',
-      description: 'Description'
-    };
+    this.book = this.bookService.getBook();
   }
 }
--- src/app/consultation/consultation.module.ts
+++ src/app/consultation/consultation.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookDisplayComponent } from './book-display/book-display.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   declarations: [BookDisplayComponent],
   exports: [BookDisplayComponent]
 })
